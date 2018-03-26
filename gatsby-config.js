0ffc26ed227f535ac0bb67025fc4aed8226f6a3e
Michal Czaplinski <mmczaplinski@gmail.com>
2018-03-26 19:08:08 +0300

    add sentry

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     {
+      resolve: "gatsby-plugin-sentry",
+      options: {
+        dsn: "https://c20f66963e9d4dbdbbe43aa59460bfa9@sentry.io/634835",
+        version: "3.24.0"
+      }
+    },
+    {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN
