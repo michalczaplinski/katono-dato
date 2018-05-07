f1df5f2adf73519873440f529205e62d84df1901
Michal Czaplinski <mmczaplinski@gmail.com>
2018-05-07 14:51:18 +0300

    Add the cache invalidation if localstorage data is stale

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
+import { connect } from "react-redux";
+import { intersection } from "lodash";
 
 import "normalize.css";
 import "../styles/index.css";
@@ -33,36 +35,61 @@ const PageContainer = styled.div`
   background-color: ${colors.backgroundColor};
 `;
 
-const Layout = ({ children, data, location, history }) => (
-  <div>
-    <Helmet>
-      <title>Katono.clothing</title>
-      <meta
-        name="description"
-        content="KATONO.CLOTHING -- thrift clothing with flair."
-      />
-      <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-      <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
-      <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
-      <link rel="manifest" href={siteManifest} />
-      <link rel="mask-icon" href={safariIcon} color="#5bbad5" />
-      <link rel="shortcut icon" href={favicon} />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-config" content={browserconfig} />
+const Layout = class extends React.Component {
+  componentDidMount() {
+    const {
+      data: { allDatoCmsClothingItem: { edges } },
+      cart: reduxCart,
+      syncCart
+    } = this.props;
 
-      <meta name="theme-color" content="#ffffff" />
-    </Helmet>
-    <Topbar
-      showBackButton={
-        location.pathname !== "/" && location.pathname !== "/success"
-      }
-      goBack={history.goBack}
-    />
-    <PageContainer>{children()}</PageContainer>
-  </div>
-);
+    const datoCart = edges.map(({ node }) => node.id);
+    const updatedCart = intersection(datoCart, reduxCart);
+    console.log(updatedCart);
+    this.props.syncCart(updatedCart);
+  }
 
-export default Layout;
+  render() {
+    const { children, location, history } = this.props;
+
+    return (
+      <div>
+        <Helmet>
+          <title>Katono.clothing</title>
+          <meta
+            name="description"
+            content="KATONO.CLOTHING -- thrift clothing with flair."
+          />
+          <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+          <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
+          <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
+          <link rel="manifest" href={siteManifest} />
+          <link rel="mask-icon" href={safariIcon} color="#5bbad5" />
+          <link rel="shortcut icon" href={favicon} />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-config" content={browserconfig} />
+
+          <meta name="theme-color" content="#ffffff" />
+        </Helmet>
+        <Topbar
+          showBackButton={
+            location.pathname !== "/" && location.pathname !== "/success"
+          }
+          goBack={history.goBack}
+        />
+        <PageContainer>{children()}</PageContainer>
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = ({ cart }) => ({ cart });
+
+const mapDispatchToProps = dispatch => ({
+  syncCart: cart => dispatch({ type: "SYNC_STORE_WITH_DATOCMS_DATA", cart })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
 
 export const query = graphql`
   query LayoutQuery {
@@ -84,6 +111,13 @@ export const query = graphql`
         }
       }
       copyright
+    }
+    allDatoCmsClothingItem(filter: { available: { eq: true } }) {
+      edges {
+        node {
+          id
+        }
+      }
     }
     allDatoCmsSocialProfile(sort: { fields: [position], order: ASC }) {
       edges {
